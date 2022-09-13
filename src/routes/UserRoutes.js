2db0902ce443af7639f7820f72411744b151b126
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-09-13 19:32:55 +0300

    Update UserRoutes.js

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -15,8 +15,8 @@ router.get("/all/:type", [verifySession, isAdmin], getAllUsers)
 router.get("/roles/all", [verifySession, isAdmin], getRoles)
 router.get("/pages", [verifySession, isAdmin], getPages)
 router.get("/:id", verifySession, getUser)
-router.delete("/:id", [verifySession, isAdmin], deleteUser)
-router.delete("/multiple", [verifySession, isAdmin], deleteMultipleUsers)
+router.delete("/delete/:id", [verifySession, isAdmin], deleteUser)
+router.delete("/delete/multiple", [verifySession, isAdmin], deleteMultipleUsers)
 router.put("/update/:id", [verifySession, isAdmin], updateUser)
 
 module.exports = router
